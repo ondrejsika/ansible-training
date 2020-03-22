6f80e63153663ed8b2cd2f6daa3dfedf3725c3e5
Ondrej Sika <ondrej@ondrejsika.com>
2020-03-22 17:18:23 +0100

    terraform - add workshop environmet on DigitalOcean

---
+++ terraform/terraform.tf
@@ -0,0 +1,97 @@
+variable "do_token" {}
+variable "cloudflare_email" {}
+variable "cloudflare_token" {}
+
+variable "vm_count" {
+  default = 1
+}
+
+provider "digitalocean" {
+  token = var.do_token
+}
+
+provider "cloudflare" {
+  version = "~> 1.0"
+  email = var.cloudflare_email
+  token = var.cloudflare_token
+}
+
+
+data "digitalocean_ssh_key" "ondrejsika" {
+  name = "ondrejsika"
+}
+
+
+// Main VM for Ansible
+
+resource "digitalocean_droplet" "ansible" {
+  count = var.vm_count
+
+  image  = "debian-10-x64"
+  name   = "ansible"
+  region = "fra1"
+  size   = "s-1vcpu-1gb"
+  ssh_keys = [
+    data.digitalocean_ssh_key.ondrejsika.id
+  ]
+}
+
+resource "cloudflare_record" "ansible" {
+  count = var.vm_count
+
+  domain = "sikademo.com"
+  name   = "ansible${count.index}"
+  value  = digitalocean_droplet.ansible[count.index].ipv4_address
+  type   = "A"
+  proxied = false
+}
+
+
+// Managed VM 0
+
+resource "digitalocean_droplet" "vm0" {
+  count = var.vm_count
+
+  image  = "debian-10-x64"
+  name   = "ansible-vm0"
+  region = "fra1"
+  size   = "s-1vcpu-1gb"
+  ssh_keys = [
+    data.digitalocean_ssh_key.ondrejsika.id
+  ]
+}
+
+resource "cloudflare_record" "vm0" {
+  count = var.vm_count
+
+  domain = "sikademo.com"
+  name   = "ansible${count.index}-vm0"
+  value  = digitalocean_droplet.vm0[count.index].ipv4_address
+  type   = "A"
+  proxied = false
+}
+
+
+// Managed VM 1
+
+resource "digitalocean_droplet" "vm1" {
+  count = var.vm_count
+
+  image  = "debian-10-x64"
+  name   = "ansible-vm1"
+  region = "fra1"
+  size   = "s-1vcpu-1gb"
+  ssh_keys = [
+    data.digitalocean_ssh_key.ondrejsika.id
+  ]
+}
+
+resource "cloudflare_record" "vm1" {
+  count = var.vm_count
+
+  domain = "sikademo.com"
+  name   = "ansible${count.index}-vm1"
+  value  = digitalocean_droplet.vm1[count.index].ipv4_address
+  type   = "A"
+  proxied = false
+}
